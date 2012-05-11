3d910ac72e79c7a896405ae17ab9b370dbce485e
Mirko Kiefer <mail@mirkokiefer.com>
2012-05-11 17:40:18 +0200

    store read/write test passes

--- store.lua
+++ store.lua
@@ -19,13 +19,16 @@ store.pullFromPeer = function(store, storeRef, peer, peerRef)
 end
 
 store.write = function(store, path, data)
-  local file = io.open(store.directory.."/"..path..".txt", "w")
+  local file = assert(io.open(store.directory.."/"..path..".txt", "w"))
   file:write(data)
   file:close()
 end
 
 store.read = function(store, path)
-
+  local file = assert(io.open(store.directory.."/"..path..".txt", "r"))
+  local data = file:read("*a")
+  file:close()
+  return data
 end
 
 return store
