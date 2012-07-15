2f1ced3089edd11a2cda2b03cf45f3c93364ed44
Mirko Kiefer <mail@mirkokiefer.com>
2012-07-15 13:58:59 +0200

    add list utilities

--- moonstore/utils.lua
+++ moonstore/utils.lua
@@ -1,6 +1,7 @@
 
 local crypto = require("crypto")
 local string = require("string")
+local table = require("table")
 local utils = {}
 
 utils.hash = function(data)
@@ -15,10 +16,54 @@ utils.deleteDirectory = function(directory)
 end
 
 utils.splitString = function(aString, sep)
-  local sep, fields = sep or ":", {}
+  local sep = sep or ":"
+  local tokens = nil
   local pattern = string.format("([^%s]+)", sep)
-  string.gsub(aString, pattern, function(c) fields[#fields+1] = c end)
-  return fields
+  string.gsub(aString, pattern, function(c) tokens = utils.listAdd(tokens, c) end)
+  return utils.listReverse(tokens)
+end
+
+utils.tableCopy = function(aTable)
+  local t2 = {}
+  for k,v in pairs(t) do
+    t2[k] = v
+  end
+  return t2
+end
+
+utils.list = function(first, ...)
+  if first == nil then return nil end
+  return {value = first, rest = utils.list(...)}
+end
+
+utils.listAdd = function(list, value)
+  return {value = value, rest = list}
+end
+
+utils.listAt = function(list, index)
+  if index == 1 then return list.value end
+  return listAt(list.rest, index-1)
+end
+
+utils.listReverse = function(list)
+  reversed = nil
+  while(list) do
+    reversed = utils.listAdd(reversed, list.value)
+    list = list.rest
+  end
+  return reversed
+end
+
+utils.listValues = function(list, maxValues)
+  array = {}
+  index = 1
+  while(list) do
+    array[index] = list.value
+    if maxValues and i >= maxValues then break end
+    list = list.rest
+    index = index + 1
+  end
+  return unpack(array)
 end
 
 return utils
--- test/test.lua
+++ test/test.lua
@@ -16,22 +16,53 @@ local function testFilestore(directory)
   filestore.delete(mystore)
 end
 
-local function testHash()
-  local data = "abc"
-  local knownHash = "a9993e364706816aba3e25717850c26c9cd0d89d"
-  assert(utils.hash(data) == knownHash, "test hash")
+local function testUtils()
+  local testHash = function ()
+    local data = "abc"
+    local knownHash = "a9993e364706816aba3e25717850c26c9cd0d89d"
+    assert(utils.hash(data) == knownHash, "test hash")
+  end
+  local testList = function()
+    list = utils.list(1,2,3)
+    assert(list.value==1 and list.rest.value==2 and list.rest.rest.value ==3)
+  end
+  local testListReverse = function()
+    list = utils.list(1,2,3)
+    reversed = utils.listReverse(list)
+    a,b,c = utils.listValues(reversed)
+    assert(a==3 and b==2 and c==1)
+  end
+  local testSplitString = function()
+    local aString = "a/bc/d"
+    local splitted = utils.splitString(aString, "/")
+    a,b,c = utils.listValues(splitted)
+    assert(a == "a" and b == "bc" and c == "d")
+  end
+  testHash()
+  testList()
+  testListReverse()
+  testSplitString()
 end
 
-local function testSplitString()
-  local aString = "a/bc/d"
-  local splitted = utils.splitString(aString, "/")
-  assert(splitted[1] == "a")
-  assert(splitted[2] == "bc")
-  assert(splitted[3] == "d")
+local function testMoonStore(directory)
+  store = ms.create(directory)
+  data1 = {
+    ["a/b"] = "value1",
+    ["a/c"] = "value2",
+    ["d"] = "value3",
+    ["a/e/f"] = "value4"
+  }
+  data2 = {
+    ["a/b"] = "value1_changed",
+    ["d"] = nil,
+    ["a/e/g"] = "new_value"
+  }
+  commit1 = ms.commit(store, nil, data1)
+  commit2 = ms.commit(store, commit1, data2)
+
 end
 
 local testFolder = "teststore"
 
 testFilestore(testFolder)
-testHash()
-testSplitString()
+testUtils()
