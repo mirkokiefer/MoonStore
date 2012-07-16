64d28cfa4928ebaccf938cf7a21d432f8831d6b8
Mirko Kiefer <mail@mirkokiefer.com>
2012-07-16 22:30:19 +0200

    store blobs, trees and commits separately

--- moonstore.lua
+++ moonstore.lua
@@ -3,46 +3,56 @@ local table = require("table")
 local filestore = require("moonstore.filestore")
 local utils = require("moonstore.utils")
 
+local readBlob = function(store, hash)
+  return store.storeModule.readBlob(store.storeObj, hash)
+end
+local writeBlob = function(store, data)
+  local hash = utils.hash(data)
+  store.storeModule.writeBlob(store.storeObj, hash, data)
+  return hash
+end
+local readCommit = function(store, hash)
+  return utils.deserializeString(store.storeModule.readCommit(store.storeObj, hash))
+end
+local writeCommit = function(store, parentCommits, tree)
+  local commit = {parents = parentCommits, tree = tree}
+  local commitSerialized = utils.serializeToString(commit)
+  local commitHash = utils.hash(commitSerialized)
+  store.storeModule.writeCommit(store.storeObj, commitHash, commitSerialized)
+  return commitHash
+end
+local readTree = function(store, hash)
+  return utils.deserializeString(store.storeModule.readTree(store.storeObj, hash))
+end
+local writeTree = function(store, tree)
+  local serialized = utils.serializeToString(tree)
+  local hash = utils.hash(serialized)
+  store.storeModule.writeTree(store.storeObj, hash, serialized)
+  return hash
+end
+
 moonstore = {}
 moonstore.new = function(directory)
   local storeBackend = filestore.new(directory)
   return {storeObj = storeBackend, storeModule = filestore}
 end
 
-local readObject = function(store, hash)
-  local data = store.storeModule.read(store.storeObj, hash)
-  return utils.deserializeString(data)
-end
-
-writeTree = function(store, oldTree, tree)
+writeChangedTree = function(store, oldTree, changedTree)
   local newTree = utils.tableCopy(oldTree)
-  for key, child in pairs(tree) do
+  for key, child in pairs(changedTree) do
     if (type(child) == "table") then
       local oldChildTree = {}
-      if(oldTree[key]) then oldChildTree = readObject(store, oldTree[key]) end
-      newTree[key] = writeTree(store, oldChildTree, child)
+      if(oldTree[key]) then oldChildTree = readTree(store, oldTree[key]) end
+      newTree[key] = writeChangedTree(store, oldChildTree, child)
     else
       if (child == false) then
         newTree[key] = nil
       else
-        local hash = utils.hash(child)
-        store.storeModule.write(store.storeObj, hash, child)
-        newTree[key] = hash
+        newTree[key] = writeBlob(store, child)
       end
     end
   end
-  local serialized = utils.serializeToString(newTree)
-  local hash = utils.hash(serialized)
-  store.storeModule.write(store.storeObj, hash, serialized)
-  return hash
-end
-
-local writeCommit = function(store, parentCommits, tree)
-  local commit = {parents = parentCommits, tree = tree}
-  local commitSerialized = utils.serializeToString(commit)
-  local commitHash = utils.hash(commitSerialized)
-  store.storeModule.write(store.storeObj, commitHash, commitSerialized)
-  return commitHash
+  return writeTree(store, newTree)
 end
 
 moonstore.commit = function(store, parentCommit, data)
@@ -50,10 +60,10 @@ moonstore.commit = function(store, parentCommit, data)
   local changedTree = utils.listToTree(dataList)
   local parentCommitTree = {}
   if (parentCommit) then
-    local parentCommitObj = readObject(store, parentCommit)
-    parentCommitTree = readObject(store, parentCommitObj.tree)
+    local parentCommitObj = readCommit(store, parentCommit)
+    parentCommitTree = readTree(store, parentCommitObj.tree)
   end
-  local treeHash = writeTree(store, parentCommitTree, changedTree)
+  local treeHash = writeChangedTree(store, parentCommitTree, changedTree)
   return writeCommit(store, {parentCommit}, treeHash)
 end
 
@@ -66,16 +76,16 @@ moonstore.paths = function(store, commit)
 end
 
 moonstore.rootSegments = function(store, commit)
-  local obj = readObject(store, commit)
-  return readObject(store, obj.tree)
+  local obj = readCommit(store, commit)
+  return readTree(store, obj.tree)
 end
 
 moonstore.childSegments = function(store, segmentHash)
-  return readObject(store, segmentHash)
+  return readTree(store, segmentHash)
 end
 
 moonstore.parentCommits = function(store, commit)
-  local obj = readObject(store, commit)
+  local obj = readCommit(store, commit)
   return obj.parents
 end
 
